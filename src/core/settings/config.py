0e3749d6f62781f36bf2a771cd6150d5fef2b52b
Adryan Ashidiq <adryanashidiq.analyst@gmail.com>
2026-04-22 11:12:29 +0000

    (FIX) Missing env key

--- src/core/settings/config.py
+++ src/core/settings/config.py
@@ -4,10 +4,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     POSTGRES_URL: str
+    ALLOWED_ORIGINS: str
     SECRET_KEY: str = secrets.token_urlsafe(32)
     # 60 minutes * 24 hours * 3 days = 3 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 3
-    
+
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 
